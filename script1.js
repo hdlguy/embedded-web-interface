f62446fdde624f1f2b2c367d05c82c40b1125528
pete <pete@hdlguy.com>
2023-12-11 22:31:45 +0000

    starting to call js functions

--- script1.js
+++ script1.js
@@ -1,2 +1,18 @@
 
-document.write("This page was last modified on " + document.lastModified);
+function printModDate () {
+    document.write("This page was modified on " + document.lastModified);
+}
+
+function showHelloAlert() {
+    alert('Hello Campers');
+}
+
+function changeBackgroundColor(newColor) {
+    document.body.style.backgroundColor = newColor;
+}
+
+function changeColors(newBackColor, newForeColor) {
+    document.body.style.backgroundColor = newBackColor;
+    document.body.style.color = newForeColor;
+}
+
